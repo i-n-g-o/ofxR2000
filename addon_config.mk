399791ea31e2a00041d26818c495009ee969024f
Ingo Randolf <ingo.randolf@servus.at>
2015-08-10 09:45:04 +0200

    add addon_config

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxR2000
+	ADDON_DESCRIPTION = Addon for using Pepperl+Fuchs laser rangefinder R2000. Based on driver written by Denis Dillenberger.
+	ADDON_AUTHOR = Ingo Randolf, Denis Dillenberger
+	ADDON_TAGS = "Hardware Interface"
+	ADDON_URL = https://github.com/i-n-g-o/ofxR2000
